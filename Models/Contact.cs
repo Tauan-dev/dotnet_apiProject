7d170bff6cab6fd52a90aae7db9b887adea020d9
Tauan Neres <tauanspider@gmail.com>
2024-11-08 12:02:17 -0300

    Models modify

--- Models/Contact.cs
+++ Models/Contact.cs
@@ -1,9 +1,9 @@
 public class Contact {
-    public int contactid {get;set;}
-    public string type {get;set;}
-    public string number  {get;set;}
+    public int? contactId {get;set;}
+    public string? type {get;set;}
+    public string? number  {get;set;}
 
     //many to one
-    public int idUser {get;set;}
-    public User User {get;set;}
+    public int? idUser {get;set;}
+    public User? User {get;set;}
 }
--- Models/Users.cs
+++ Models/Users.cs
@@ -1,12 +1,12 @@
 public class User  {
-    public int id { get; set; }
-    public string name { get; set; }
-    public string birthdate{ get; set; }
-    public string email { get; set; }
+    public int? userId { get; set; }
+    public string? name { get; set; }
+    public string? birthdate{ get; set; }
+    public string? email { get; set; }
 
     // relations one to many
 
-    public ICollection<Contact> Contatcs {get; set; }
+    public ICollection<Contact>? Contatcs {get; set; }
     
-    public ICollection<Order>  Orders {get;set;}
+    public ICollection<Order>?  Orders {get;set;}
 }
